9423d8b6dc9649e3b5ff675f29729a74b108e345
joseljg <joseljg@gmail.com>
2022-11-04 17:03:24 +0100

    version 2

--- app/src/main/java/es/joseljg/formularioestudiante2223/Activity2.java
+++ app/src/main/java/es/joseljg/formularioestudiante2223/Activity2.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import es.joseljg.formularioestudiante2223.clases.Estudiante;
 
 public class Activity2 extends AppCompatActivity {
+
+    private Estudiante e = null;
+    private TextView txt_dni_estudiante = null;
+    private TextView txt_nombre_estudiante = null;
+    private TextView txt_curso_estudiante = null;
+    private TextView txt_fecha_estudiante = null;
+    private TextView txt_hora_estudiante = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +27,26 @@ public class Activity2 extends AppCompatActivity {
         Intent intent =  getIntent();
         if(intent != null)
         {
-            Estudiante e = (Estudiante) intent.getSerializableExtra(MainActivity.EXTRA_ESTUDIANTE);
-            Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show();
+            e = (Estudiante) intent.getSerializableExtra(MainActivity.EXTRA_ESTUDIANTE);
+          //  Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show();
+        }
+        if(e != null)
+        {
+            txt_dni_estudiante = (TextView) findViewById(R.id.txt_dni_estudiante);
+            txt_dni_estudiante.setText("DNI: " + e.getDni());
+
+            txt_nombre_estudiante = (TextView) findViewById(R.id.txt_nombre_estudiante);
+            txt_nombre_estudiante.setText("NOMBRE: " + e.getNombre());
+
+            txt_curso_estudiante = (TextView) findViewById(R.id.txt_curso);
+            txt_curso_estudiante.setText("CURSO: " + e.getCurso());
+
+            txt_fecha_estudiante = (TextView) findViewById(R.id.txt_fecha_estudiante);
+            txt_fecha_estudiante.setText("FECHA: "+ e.getFecha_nacimiento());
+
+            txt_hora_estudiante = (TextView) findViewById(R.id.txt_hora_estudiante);
+            txt_hora_estudiante.setText("HORA: " + e.getHora_tutoria());
+
         }
     }
 }
